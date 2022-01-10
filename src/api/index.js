c97e59dc02e61a703e6f806f39142915c2b31426
ernest <ernest_everstov@mail.ru>
2022-01-10 14:46:18 +0900

    changed api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const testURL = 'http://127.0.0.1:8000/'
-const publicURL = 'http://proekt-hope.ru/'
+const publicURL = 'http://liver.mondino.tech/'
 
 const api = (url) => {
     const instance = axios.create({
